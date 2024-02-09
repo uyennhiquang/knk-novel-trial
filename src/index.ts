eefb0d2a6b445f550979243246d19973bdca6fa0
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-02-08 19:45:08 -0500

    Add dark mode detector

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { GameDOM } from "./dom/game-dom"
 import "../assets/styles/reset.css";
 import "../assets/styles/styles.css";
 
+const root = document.documentElement;
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  // dark mode
+  root.className = 'dark';
+}
+else {
+  root.className = 'light';
+}
+
 const MenuDOM = (() => {
   const homeScreen = document.getElementById("screen--home");
   const startButton = document.getElementById("button--start");
