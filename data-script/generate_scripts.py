38aade422adc66b72cb14763b596391d6b86d257
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-12-25 20:01:13 -0500

    Add script that runs all db scripts

---
+++ data-script/generate_scripts.py
@@ -0,0 +1,7 @@
+import generate_text
+
+def main():
+  generate_text.main()
+
+if __name__ == "__main__":
+  main()
--- data-script/generate_text.py
+++ data-script/generate_text.py
@@ -234,28 +234,35 @@ ost = [
     }
 ]
 
-for novel_index in range(2):
-    current_title = TITLES[novel_index]
-    novel_dict = {
-        "title": current_title,
-        "content": []
+def generate_text():
+    for novel_index in range(2):
+        current_title = TITLES[novel_index]
+        novel_dict = {
+            "title": current_title,
+            "content": []
+        }
+        for chapter_index in range(TITLES_DICT[current_title]):
+            chapter = []
+            file = f"{current_title}/ch{chapter_index}.txt"
+            with open(file) as f:
+                for line in f:
+                    sentences = re.findall(regex, line)
+                    paragraph = [{"en": sentence, "jp": ""}
+                                for sentence in sentences]
+                    chapter.append(paragraph)
+            novel_dict["content"].append(chapter)
+        knk_text.append(novel_dict)
+
+    json_data = {
+        "knk_text": knk_text
     }
-    for chapter_index in range(TITLES_DICT[current_title]):
-        chapter = []
-        file = f"{current_title}/ch{chapter_index}.txt"
-        with open(file) as f:
-            for line in f:
-                sentences = re.findall(regex, line)
-                paragraph = [{"en": sentence, "jp": ""}
-                             for sentence in sentences]
-                chapter.append(paragraph)
-        novel_dict["content"].append(chapter)
-    knk_text.append(novel_dict)
-
-json_data = {
-    "knk_text": knk_text
-}
 
 
-with open("knk_text.json", "w", encoding="utf-8") as f:
-    json.dump(json_data, f, ensure_ascii=False, indent=2)
+    with open("knk_text.json", "w", encoding="utf-8") as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=2)
+
+def main():
+    generate_text()
+
+if __name__ == "__main__":
+    main()
